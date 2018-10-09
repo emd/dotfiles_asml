b4d08eb2308e17f64beb192d1af240de6a08aba8
Evan Davis <evan.davis@asml.com>
2018-10-08 17:08:28 -0700

    Symlink repo dotfiles to $HOME

---
+++ makesymlinks.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#####################################################################
+# This script symlinks dotfiles from in $HOME/dotfiles_asml to $HOME.
+#####################################################################
+
+# dotfiles directory
+dir=$HOME/dotfiles_asml
+
+# old dotfiles backup directory
+olddir=$HOME/dotfiles_old
+
+# list of dot files/folders to symlink
+# NOTE: Dots are *suppressed* and are manually prepended
+# in the linking commands below. This allows us to easily
+# list contents of dotfiles w/o needing to use -a flag.
+files="bashrc"
+
+echo "Creating $olddir for backup of any existing dotfiles in \$HOME"
+mkdir -p $olddir
+
+for file in $files; do
+    # Move existing dotfile to $olddir
+    if [ -e ~/.$file ]; then
+        echo "Moving .$file from \$HOME to $olddir"
+        mv ~/.$file $olddir/.
+    fi
+
+    # Create symlink to new dotfile
+    echo "Creating a symlink to $file in \$HOME"
+    ln -s $dir/$file ~/.$file
+done
